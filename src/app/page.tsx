04c8ddf2619826212a70819617a979e0627cdb2c
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-08-27 12:17:56 +0545

    about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import { getHomeData, getOptionsData } from '@/services/network_requests';
 import HomeBanner from '@/components/Banners/HomeBanner';
 import FeaturedCategory from '@/components/FeaturedCategory/FeaturedCategory';
 import FeaturedPackages from '@/components/FeaturedPackages/FeaturedPackages';
+import AboutUs from '@/components/AboutUs/AboutUs';
+
+const BASE_URL = process.env.BASE_URL;
+const IMAGE_URL = process.env.IMAGE_URL;
+
 
 export interface FeaturedCategoryItem {
   id: string | number;
@@ -33,6 +38,30 @@ export interface OptionsData {
   [key: string]: any;
 }
 
+export async function generateMetadata() {
+  const response = await getHomeData();
+  const data = await response?.data?.data;
+  return {
+    title: data?.pagecontent?.meta?.meta_title,
+    description: data?.pagecontent?.meta?.meta_description,
+    alternates: {
+      canonical: process.env.CANONICAL_BASE,
+    },
+    openGraph: {
+      title: data?.pagecontent?.meta?.meta_title,
+      description: data?.pagecontent?.meta?.meta_description,
+      url: BASE_URL,
+      images: [
+        {
+          url: IMAGE_URL + data?.pagecontent?.carousel?.content[0]?.full_path,
+          width: 1920,
+          height: 700,
+        },
+      ],
+    },
+  };
+}
+
 export default async function Home() {
   const response = await getHomeData();
   const data: HomeData = response?.data?.data;
@@ -51,6 +80,7 @@ export default async function Home() {
         limit={3}
         data={data?.featured_packages || []}
       />
+      <AboutUs data={data?.pagecontent} />
     </main>
   );
 }
---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import PrimaryButton from '../Buttons/PrimaryButton';
+import Image from 'next/image';
+
+import aboutus1 from '../../../public/AboutUs/aboutus1.png';
+
+const features = [
+  'Safety First, Always Guaranteed',
+  'Trusted Travel Guide Service',
+  'Expertise and experience',
+  'time and stress savings',
+];
+
+const AboutUs = ({ data }) => {
+  return (
+    <div className="about-us common-box pt-0">
+      <div className="container grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="about-us-body">
+          <div className="about-us-section">
+            <div className="title">
+              <span className=" !justify-start">
+                <svg
+                  className="icon"
+                  width="26"
+                  height="28"
+                  fill="currentColor">
+                  <use xlinkHref="/icons.svg#title-top" />
+                </svg>
+                Made Just For You
+              </span>
+              <h2
+                className="company_title"
+                dangerouslySetInnerHTML={{ __html: data?.page_title }}
+              />
+            </div>
+            <article
+              className="description"
+              dangerouslySetInnerHTML={{ __html: data?.page_description }}
+            />
+          </div>
+
+          <div className="feature-section mt-[18px] flex flex-col gap-6 md:flex-row md:items-center">
+            <ul className="features-list grid grid-cols-1 md:grid-cols-2 gap-6">
+              {features.map((feature, index) => {
+                const slug = feature
+                  .toLowerCase()
+                  .replace(/[^a-z0-9]+/g, '-')
+                  .replace(/(^-|-$)/g, '');
+
+                return (
+                  <li
+                    key={index}
+                    className="feature-item flex items-center gap-[15px] px-6 py-3 bg-[#FFF9E2] first:bg-primary/10 last:bg-primary/10 rounded-2xl">
+                    <i className="h-[56px] w-[56px] flex items-center justify-center rounded-full bg-white shrink-0">
+                      <svg
+                        className="text-primary shrink-0"
+                        width="26"
+                        height="26"
+                        fill="currentColor">
+                        <use xlinkHref={`/icons.svg#${slug}`} />
+                      </svg>
+                    </i>
+                    <span className="font-bold text-headings">{feature}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div className="mt-8">
+            <PrimaryButton
+              href="/our-story"
+              label="Read More About Us"
+            />
+          </div>
+        </div>
+        <div className="about-us-media hidden lg:block relative ">
+          <figure className=" !bg-page-bg image-slot before:aspect-[628/560] w-full">
+            <Image
+              src={aboutus1}
+              alt="Hero"
+              width={628}
+              height={560}
+              className="object-cover"
+              sizes="(max-width: 628px) 100vw, 628px"
+              fetchPriority="high"
+            />
+          </figure>
+          <figcaption className=' bg-primary w-fit text-white flex items-center justify-center gap-2 py-2 px-3 absolute bottom-6 rounded-[10px] border-4 border-page-bg right-0 z-10'>
+            <p className='text-[35px] leading-1 font-bold'>05</p>
+            <p className='text-semibold leading-[19px]'>Years of <br/> experience</p>
+          </figcaption>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
