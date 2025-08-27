5538c34aa74a9bdd5c5de441fccd155c30fde767
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-08-27 08:28:27 +0545

    git init

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['var(--primary)'],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
