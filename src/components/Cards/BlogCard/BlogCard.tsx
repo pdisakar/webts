28c67f464b4bcdf1f049c35ea90fefed675d859b
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-08-27 15:40:24 +0545

    primary btn fixed

--- src/components/Cards/BlogCard/BlogCard.tsx
+++ src/components/Cards/BlogCard/BlogCard.tsx
@@ -25,41 +25,43 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   return (
-    <li className="blog-card flex h-full gap-6 items-center">
-      <figure className="card-img h-full rounded-[4px]">
-        <Link
-          className="image-slot before:aspect-[235/156] min-w-[235px] h-full rounded-[4px]"
-          href={`blog/${blog.urlinfo.url_slug}`}
-          aria-label={`View image for ${blog.title}`}>
-          <Image
-            src={IMAGE_URL + blog?.featured?.full_path}
-            alt={blog.title}
-            width={235}
-            height={156}
-            className="object-cover rounded-[4px] bg-page-bg hover:scale-110 transition-transform duration-400 ease-in h-full"
-            sizes="auto, (max-width: 235px), 235px"
-            fetchPriority="high"
-          />
-        </Link>
-      </figure>
-      <figcaption className="blog-details ">
-        <Link
-          className="package-title"
-          href={`blog/${blog.urlinfo.url_slug}`}
-          aria-label={`View details for ${blog.title}`}>
-          <h3 className="text-xl text-headings font-black leading-[160%] hover:text-primary hover:underline transition-all duration-150 ease-linear">
-            {blog.title}
-          </h3>
-        </Link>
-        <div className="about-blog flex items-center gap-3 mt-4">
-          <span className="blog-author capitalize text-primary font-semibold">
-            {blog.authors?.[0]?.name || 'Unknown Author'}
-          </span>
-          <span className="blog-date capitalize font-semibold">
-            {format(new Date(blog.blog_date), 'dd MMM yyyy')}
-          </span>
-        </div>
-      </figcaption>
+    <li className="lg:[&>.item]:grid lg:[&>.item]:items-center [&>.item>figcaption]:pt-2 lg:[&>.item]:grid-cols-5 lg:[&>.item]:gap-5 lg:[&>.item>figure]:col-span-2 lg:[&>.item>figcaption]:col-span-3 lg:[&>.item>figcaption]:pt-0">
+      <div className="item transation-all overflow-hidden group">
+        <figure className="card-img h-full rounded-[4px]">
+          <Link
+            className="image-slot before:aspect-[235/156] min-w-[235px] h-full rounded-[4px]"
+            href={`blog/${blog.urlinfo.url_slug}`}
+            aria-label={`View image for ${blog.title}`}>
+            <Image
+              src={IMAGE_URL + blog?.featured?.full_path}
+              alt={blog.title}
+              width={235}
+              height={156}
+              className="object-cover rounded-[4px] bg-page-bg hover:scale-110 transition-transform duration-400 ease-in h-full"
+              sizes="auto, (max-width: 235px), 235px"
+              fetchPriority="high"
+            />
+          </Link>
+        </figure>
+        <figcaption className="blog-details">
+          <Link
+            className="package-title"
+            href={`blog/${blog.urlinfo.url_slug}`}
+            aria-label={`View details for ${blog.title}`}>
+            <h3 className="text-xl text-headings font-black leading-[160%] hover:text-primary hover:underline transition-all duration-150 ease-linear">
+              {blog.title}
+            </h3>
+          </Link>
+          <div className="about-blog flex items-center gap-3 mt-4">
+            <span className="blog-author capitalize text-primary font-semibold">
+              {blog.authors?.[0]?.name || 'Unknown Author'}
+            </span>
+            <span className="blog-date capitalize font-semibold">
+              {format(new Date(blog.blog_date), 'dd MMM yyyy')}
+            </span>
+          </div>
+        </figcaption>
+      </div>
     </li>
   );
 };
