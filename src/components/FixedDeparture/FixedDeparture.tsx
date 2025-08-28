af7f4cb0b9a1736fec38d997ad97bbe5071896f8
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-08-28 12:12:03 +0545

    fixed departure is done

--- src/components/FixedDeparture/FixedDeparture.tsx
+++ src/components/FixedDeparture/FixedDeparture.tsx
@@ -29,7 +29,7 @@ const FixedDeparture: React.FC<FixedDepartureProps> = ({ data, duration }) => {
   ).sort((a, b) => {
     const [yearA, monthA] = a.split('-').map(Number);
     const [yearB, monthB] = b.split('-').map(Number);
-    return yearA - yearB || monthA - monthB; 
+    return yearA - yearB || monthA - monthB;
   });
 
   const currentDate = new Date();
@@ -50,7 +50,7 @@ const FixedDeparture: React.FC<FixedDepartureProps> = ({ data, duration }) => {
     );
 
   return (
-    <div className="overflow-x-auto w-full">
+    <div className="w-full">
       <ul className="flex flex-wrap gap-2 mb-4">
         {monthYears.map(key => {
           const [year, month] = key.split('-').map(Number);
@@ -69,40 +69,41 @@ const FixedDeparture: React.FC<FixedDepartureProps> = ({ data, duration }) => {
         })}
       </ul>
 
-      <table className="w-full min-w-[600px] border-collapse [&_tr:nth-child(odd)]:bg-primary/5 [&_td]:px-6 [&_td]:py-3 [&_p]:text-sm [&_p]:pb-1 [&_p]:text-gray-600 [&_p]:font-bold [&_p]:text-headings [&_span]:font-medium [&_td:last-child]:text-right">
-        <tbody>
-          {filteredDepartures.map(departure => {
-            const startDate = new Date(departure.departure_date);
-            const finalDate = duration
-              ? addDays(startDate, Number(duration) - 1)
-              : startDate;
+      <ul className="[&>li]:px-6 [&>li]:py-3 [&>li]:rounded-lg [&>li]:bg-primary/5 [&>li]:flex [&>li]:flex-col [&>li]:sm:flex-row [&>li]:sm:justify-between [&>li]:sm:items-center [&>li]:gap-2 [&>li>div>p]:text-sm [&>li>div>p]:text-gray-600 [&>li>div>p]:font-bold [&>li>div>span]:font-medium [&>li>div>span]:text-headings space-y-2">
+        {filteredDepartures.map(departure => {
+          const startDate = parseISO(departure.departure_date);
+          const finalDate = duration
+            ? addDays(startDate, Number(duration) - 1)
+            : startDate;
 
-            return (
-              <tr key={departure.id}>
-                <td>
-                  <p>Starting From:</p>
-                  <span>{format(startDate, 'do MMM yyyy')}</span>
-                </td>
-                <td>
-                  <p>End At:</p>
-                  <span>{format(finalDate, 'do MMM yyyy')}</span>
-                </td>
-                <td className="capitalize">
-                  <p>Status</p>
-                  <span>{departure.departure_status}</span>
-                </td>
-                <td>
-                  <Link
-                    href="#"
-                    className="inline-block px-4 py-[6px] border border-primary/50 bg-primary/10 text-primary rounded-md hover:bg-primary hover:text-white text-center">
-                    Book Now
-                  </Link>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          return (
+            <li key={departure.id}>
+              <div>
+                <p>Starting From:</p>
+                <span>{format(startDate, 'do MMM yyyy')}</span>
+              </div>
+
+              <div>
+                <p>End At:</p>
+                <span>{format(finalDate, 'do MMM yyyy')}</span>
+              </div>
+
+              <div className="capitalize">
+                <p>Status</p>
+                <span>{departure.departure_status}</span>
+              </div>
+
+              <div>
+                <Link
+                  href="#"
+                  className="inline-block px-4 py-[6px] border border-primary/50 bg-primary/10 text-primary rounded-md hover:bg-primary hover:text-white text-center">
+                  Book Now
+                </Link>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
