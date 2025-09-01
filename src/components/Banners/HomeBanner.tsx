3686bca8a54995b2183808a6dc30966ed2d1db53
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-09-01 15:30:49 +0545

    home banner fixed

--- src/components/Banners/HomeBanner.tsx
+++ src/components/Banners/HomeBanner.tsx
@@ -11,7 +11,7 @@ interface HomeBannerProps {
 const HomeBanner: React.FC<HomeBannerProps> = ({ optionalData }) => {
   return (
     <section className="hero-section px-0 lg:px-6">
-      <figure className="image-slot before:aspect-[1856/750] w-full mx-auto">
+      <figure className="image-slot before:aspect-[1856/750] w-full mx-auto relative">
         <Image
           src="/banner.webp"
           alt="Hero"
@@ -24,6 +24,14 @@ const HomeBanner: React.FC<HomeBannerProps> = ({ optionalData }) => {
            (max-width: 1280px) 90vw,
            1856px"
         />
+        <figcaption className="absolute top-1/2 left-1/2 z-10 text-center transform -translate-x-1/2 -translate-y-1/2">
+          <span className="text-[15px] font-semibold text-white flex items-center justify-center mb-1 gap-2">
+            Experience the thrill of nature
+          </span>
+          <h2 className="text-[clamp(28px,5vw,52px)] text-white font-bold text-center">
+            Embark on Your Next Great Adventure Here
+          </h2>
+        </figcaption>
       </figure>
 
       <div className="home-search bg-secondary rounded-b-[20px] hidden lg:block">
