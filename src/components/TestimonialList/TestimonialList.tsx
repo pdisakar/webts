08515a12f856ea96202796040112a6940108c738
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-09-01 15:06:54 +0545

    review_date added

--- src/components/TestimonialList/TestimonialList.tsx
+++ src/components/TestimonialList/TestimonialList.tsx
@@ -8,7 +8,7 @@ import { PRODUCTION_SERVER, SITE_KEY } from '@/lib/constants'; // Adjust the imp
 export interface Testimonial {
   id?: string | number;
   full_name: string;
-  created_at: string | Date;
+  review_date: string | Date;
   review: string;
   urlinfo: {
     url_title: string;
